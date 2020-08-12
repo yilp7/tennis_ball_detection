f72e524be85ebff78e7c147a9cb5128c36d00d81
Yilun Yao <yaoyil@Yiluns-MacBook-Pro.local>
2020-08-12 09:09:31 -0400

    circle hough

--- ball_identify.cpp
+++ ball_identify.cpp
@@ -1,157 +1,109 @@
 #include <stdio.h>
 #include <iostream>
-// use VideoCapture
-#include "opencv2/videoio.hpp"
 // use namedWindow, createTrackbar, destroyAllWindows
 #include "opencv2/highgui.hpp"
-// use FileStorage, Mat, Scalar, inRange, Point2f, cvRound
+// use Mat, Scalar, inRange, Point2f, cvRound
 #include "opencv2/core.hpp"
 // use cvtColor, GaussianBlur, HoughCircle, circle
+// and getStructuringElement, morphologyEx
 #include "opencv2/imgproc.hpp"
 // #include <opencv2/core.hpp>
 
 using namespace std;
 using namespace cv;
 
+void find_balls(char* filename, char* out_name, Scalar hsv_min, Scalar hsv_max, Mat se1, Mat se2);
+
 int main(int argc, char** argv){
 
-	// parameters of thte image working on
-	int height, width;
-	int t1min, t2min, t3min, t1max, t2max, t3max;
+	char* filename = (char*)malloc(50*sizeof(char));
+	char* out_name = (char*)malloc(50*sizeof(char));
 
-	Mat threshold = Mat(2, 3, CV_32FC1);
-
-	FileStorage temp = FileStorage("threshold_matrix", FileStorage::READ);
-
-	// load previous values of the threshold if exist
-	if(temp.isOpened()){
-		temp["threshold_matrix"] >> threshold;
-		cout << threshold << endl;
-		// t1min = threshold.at<int>(0, 0); t2min = threshold.at<int>(0, 1); t3min = threshold.at<int>(0, 2);
-		// t1max = threshold.at<int>(1, 0); t2max = threshold.at<int>(1, 1); t3max = threshold.at<int>(1, 2);
-	}
-	temp.release();
-
-	// index = 0 opens default camera
-	// VideoCapture capture = VideoCapture(0);
-	// if(!capture.isOpened()){
-	// 	printf("ERROR: capture is NULL\n");
-	// 	return -1;
-	// }
-
-	Mat frame = imread(argv[1], IMREAD_COLOR);
-	// capture.read(frame);
-
-	// create the window where imaged will be shown
-	namedWindow("Camera", WINDOW_AUTOSIZE);
-	namedWindow("HSV", WINDOW_AUTOSIZE);
-	namedWindow("F1", WINDOW_AUTOSIZE);
-	namedWindow("F2", WINDOW_AUTOSIZE);
-	namedWindow("F3", WINDOW_AUTOSIZE);
-
-	// create trackbars
-	createTrackbar("t1min", "F1", &t1min, 260 , NULL);
-	createTrackbar("t1max", "F1", &t1max, 260,  NULL);
-	createTrackbar("t2min", "F2", &t2min, 260 , NULL);
-	createTrackbar("t2max", "F2", &t2max, 260,  NULL);
-	createTrackbar("t3min", "F3", &t3min, 260 , NULL);
-	createTrackbar("t3max", "F3", &t3max, 260,  NULL);
+	// windows for image display
+	// namedWindow("input", WINDOW_KEEPRATIO);
+	// namedWindow("HSV", WINDOW_KEEPRATIO);
+	// namedWindow("after color filtering", WINDOW_KEEPRATIO);
+	// resizeWindow("input", 800, 800*ratio);
+	// resizeWindow("HSV", 800, 800*ratio);
+	// resizeWindow("after color filtering", 800, 800);
 
 	// load threshold from slider bars in the 2 parameters
-	Scalar hsv_min = Scalar(t1min, t2min, t3min, 0);
-	Scalar hsv_max = Scalar(t1max, t2max, t3max, 0);
+	Scalar hsv_min = Scalar(0.11*256, 0.60*256, 0.20*256, 0);
+	Scalar hsv_max = Scalar(0.25*256, 1.00*256, 1.00*256, 0);
 
-	// image data
-	height = frame.rows;
-	width = frame.cols;
+	// TO-DO: magic numbers
+	Mat se1 = getStructuringElement(MORPH_RECT, cv::Size(11, 11));
+	Mat se2 = getStructuringElement(MORPH_RECT, cv::Size(21, 21));
 
-	// initialize different images
-	Mat hsv_frame = Mat(height, width, CV_32FC1);
-	Mat mask = Mat(height, width, CV_32FC1);
-	Mat mask1 = Mat(height, width, CV_32FC1);
-	Mat mask2 = Mat(height, width, CV_32FC1);
-	Mat mask3 = Mat(height, width, CV_32FC1);
-	Mat filtered = Mat(height, width, CV_32FC1);
-
-	int c = 1;
-	while(c--){
-		// update threshold from slider bars
-		hsv_min = Scalar(t1min, t2min, t3min, 0);
-		hsv_max = Scalar(t1max, t2max, t3max, 0);
-
-		// get 1 frame
-		// if(!capture.read(frame)){
-		// 	printf("ERROR: frame is NULL\n");
-		// 	break;
-		// }
-
-		// convert color space to HSV (much easier to filter colors)
-		cvtColor(frame, hsv_frame, COLOR_BGR2HSV);
-
-		// filter out colors out of range
-		inRange(hsv_frame, hsv_min, hsv_max, mask);
-
-		// for visual purpose
-		// --------start--------
-		// split(split image into 3 1-D images)
-		vector<Mat> masks = {mask1, mask2, mask3};
-		split(hsv_frame, masks);
-
-		// filter out colors out of range
-		inRange(mask1, Scalar(t1min, 0, 0, 0), Scalar(t1max, 0, 0, 0), mask1);
-		inRange(mask2, Scalar(t2min, 0, 0, 0), Scalar(t2max, 0, 0, 0), mask2);
-		inRange(mask3, Scalar(t3min, 0, 0, 0), Scalar(t3max, 0, 0, 0), mask3);
-		// ---------end---------
-
-		// memory for hough circles
-
-		// hough detector works better with some smoothing of the image
-		// TO-DO sigma_x, sigma_y
-		GaussianBlur(mask, mask, cv::Size(9, 9), 0, 0);
-
-		// hough transform to detect circle
-		vector<Vec3f> circles;
-		HoughCircles(mask, circles, HOUGH_GRADIENT, 2,
-			mask.rows/4, 200, 100);
-
-		for(int i = 0; i < circles.size(); i++){
-			// get the center and radius
-			Point2f center(circles[i][0], circles[i][1]);
-			float radius = circles[i][2];
-			printf("ball: x=%f y=%f r=%f\n", center.x, center.y, radius);
-
-			// draw the circle center
-			circle(frame, center, 2, Scalar(0,255,0), -1, 8, 0);
-			// draw the circle outline
-			circle(frame, center, radius, Scalar(0,0,255), 2, 8, 0);
-		}
-
-		imshow("input", frame); // original input with detected ball overlay
-		// for test purpose
-		// --------start--------
-		imshow("HSV", hsv_frame); // original input in HSV color space
-		imshow("after color filtering", mask); // the input after color filtering
-		// individual filters
-		// imshow("F1", mask1);
-		// imshow("F2", mask2);
-		// imshow("F3", mask3);
-
-		waitKey(0);
+	while(scanf("%s", filename) != EOF){
+		find_balls(filename, out_name, hsv_min, hsv_max, se1, se2);
 	}
 
-	// save threshold values
-	FileStorage out = FileStorage("threshold_matrix", FileStorage::WRITE);
-	out << "threshold_matrix" << threshold;
-
-	// release memory
-	out.release();
-	// capture.release();
-	destroyAllWindows();
+	// TO-DO: release memory
+	// free();
+	// destroyAllWindows();
 
 	return 0;
 }
 
+void find_balls(char* filename, char* out_name, Scalar hsv_min, Scalar hsv_max, Mat se1, Mat se2){
+	// load image
+	Mat frame = imread(filename, IMREAD_COLOR);
+	int height = frame.rows, width = frame.cols;
+	float ratio = height*1.0/width;
 
+	// convert to HSV format
+	Mat hsv_frame = Mat(height, width, CV_8UC3);
+	Mat mask = Mat(height, width, CV_8UC1);
+
+	// convert color space to HSV (much easier to filter colors)
+	cvtColor(frame, hsv_frame, COLOR_BGR2HSV);
+	// imwrite("hsv.jpg", hsv_frame);
+
+	// filter out colors out of range
+	inRange(hsv_frame, hsv_min, hsv_max, mask);
+
+	// smooth the image: Gaussian blur
+	// TO-DO: sigma_x, sigma_y
+	GaussianBlur(mask, mask, cv::Size(15, 15), 0, 0);
+
+	// remove the curving line on the move
+	morphologyEx(mask, mask, MORPH_CLOSE, se2);
+	morphologyEx(mask, mask, MORPH_OPEN, se1);
+
+	// hough transform to detect circle
+	vector<Vec3f> circles;
+	HoughCircles(mask, circles, HOUGH_GRADIENT, 4,
+		mask.rows/10, 100, 40, 0, 100);
+
+	for(int i = 0; i < circles.size(); i++){
+		// get the center and radius
+		Point2f center(circles[i][0], circles[i][1]);
+		float radius = circles[i][2];
+		// printf("ball: x=%f y=%f r=%f\n", center.x, center.y, radius);
+
+		// TO-DO: check if is false circle
+
+
+		// draw the circle center
+		circle(frame, center, 2, Scalar(0,255,0), -1, 8, 0);
+		// draw the circle outline
+		circle(frame, center, radius, Scalar(0,0,255), 2, 8, 0);
+	}
+
+	sprintf(out_name, "out/%s", strrchr(filename, '/')+1);
+	imwrite(out_name, frame);
+	sprintf(out_name, "mask/%s", strrchr(filename, '/')+1);
+	Mat out_mask = Mat(800, 800, CV_8UC1);
+	resize(mask, out_mask, cv::Size(800, 800));
+	imwrite(out_name, out_mask);
+	// imshow("input", frame); // original input with detected ball overlay
+	// for test purpose
+	// imshow("HSV", hsv_frame); // original input in HSV color space
+	// imshow("after color filtering", mask); // the input after color filtering
+
+	waitKey(0);
+}
 
 
 
